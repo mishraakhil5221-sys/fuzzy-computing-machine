9bc82b87b5bfa3aa301aaa1b8d13195dd02822cf
mishraakhil5221-sys <mishraakhil5221@gmail.com>
2025-12-12 11:43:57 +0530

    Update colors.dart

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Colors.green;
-  static const glass = Colors.white24;
+  static const primary = Color(0xFF1DB954); // Spotify green
+  static const glass = Colors.white24; // semi-transparent for glass effect
+  static const background = Color(0xFF121212); // dark background
+  static const cardShadow = Colors.black54;
 }
