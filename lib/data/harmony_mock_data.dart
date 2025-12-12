49d0a5cb9aed284de3f9275dad4480bc784f0ac3
mishraakhil5221-sys <mishraakhil5221@gmail.com>
2025-12-12 11:22:40 +0530

    Create harmony_mock_data.dart

---
+++ lib/data/harmony_mock_data.dart
@@ -0,0 +1,11 @@
+final playlists = [
+  {'title': 'Top Hits', 'image': 'https://picsum.photos/200?1'},
+  {'title': 'Chill Vibes', 'image': 'https://picsum.photos/200?2'},
+  {'title': 'Workout', 'image': 'https://picsum.photos/200?3'},
+];
+
+final songs = [
+  {'title': 'Song 1', 'artist': 'Artist A', 'lyrics': 'La la la...'},
+  {'title': 'Song 2', 'artist': 'Artist B', 'lyrics': 'Na na na...'},
+  {'title': 'Song 3', 'artist': 'Artist C', 'lyrics': 'Da da da...'},
+];
