b3c4ef16eac42e625114643cdd91b1d6adc71f49
mishraakhil5221-sys <mishraakhil5221@gmail.com>
2025-12-12 11:33:17 +0530

    Update mini_player.dart

--- lib/ui/widgets/mini_player.dart
+++ lib/ui/widgets/mini_player.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+import '../screens/now_playing_screen.dart';
 
-class MiniPlayer extends StatelessWidget {
+class MiniPlayer extends StatefulWidget {
+  final int songIndex;
+  MiniPlayer({this.songIndex = 0});
+
+  @override
+  _MiniPlayerState createState() => _MiniPlayerState();
+}
+
+class _MiniPlayerState extends State<MiniPlayer> {
+  bool isPlaying = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      color: Colors.grey[900],
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text('Song Name', style: TextStyle(color: Colors.white)),
-          IconButton(
-            icon: Icon(Icons.play_arrow, color: Colors.white),
-            onPressed: () {},
+    return GestureDetector(
+      onTap: () {
+        // Navigate to Now Playing screen
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => NowPlayingScreen(songIndex: widget.songIndex),
           ),
-        ],
+        );
+      },
+      child: Container(
+        height: 60,
+        color: Colors.grey[900],
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Song Name', style: TextStyle(color: Colors.white)),
+            IconButton(
+              icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow, color: Colors.white),
+              onPressed: () {
+                setState(() {
+                  isPlaying = !isPlaying;
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
