2d43e613c840f8bac324ead5a6b97ed11215f7b4
mishraakhil5221-sys <mishraakhil5221@gmail.com>
2025-12-12 11:30:12 +0530

    Create LibraryScreen with placeholder text

---
+++ lib/ui/screens/library_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LibraryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Text(
+          "Library Screen Coming Soon",
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
