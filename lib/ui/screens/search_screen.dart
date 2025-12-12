ce614d65e3ae78c44dfc77c2abfa4154c5da1760
mishraakhil5221-sys <mishraakhil5221@gmail.com>
2025-12-12 11:29:50 +0530

    Create SearchScreen widget with placeholder text

---
+++ lib/ui/screens/search_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Text(
+          "Search Screen Coming Soon",
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
