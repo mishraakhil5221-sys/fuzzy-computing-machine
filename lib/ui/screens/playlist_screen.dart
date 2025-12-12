31263fc36c7ba3a9c99d10bc50a128b8c6de88d5
mishraakhil5221-sys <mishraakhil5221@gmail.com>
2025-12-12 11:29:31 +0530

    Create PlaylistScreen with placeholder text

---
+++ lib/ui/screens/playlist_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PlaylistScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Text(
+          "Playlist Screen Coming Soon",
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
