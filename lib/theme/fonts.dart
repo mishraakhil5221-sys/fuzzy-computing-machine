7e5311c716b14c71e4c5b51add559c62a7e2ce35
mishraakhil5221-sys <mishraakhil5221@gmail.com>
2025-12-12 11:44:37 +0530

    Update fonts.dart

--- lib/theme/fonts.dart
+++ lib/theme/fonts.dart
@@ -1,2 +1,10 @@
-// Optional Google Fonts
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+class AppFonts {
+  static TextStyle title(double size, [Color color = Colors.white]) =>
+      GoogleFonts.montserrat(fontSize: size, fontWeight: FontWeight.bold, color: color);
+
+  static TextStyle subtitle(double size, [Color color = Colors.grey]) =>
+      GoogleFonts.montserrat(fontSize: size, fontWeight: FontWeight.w500, color: color);
+}
